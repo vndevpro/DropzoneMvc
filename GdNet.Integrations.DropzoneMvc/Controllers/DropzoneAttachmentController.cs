76206f4767d48fed91dfae3f1d0d10a796f2e3c9
juanonsoftware <huanhvhd@gmail.com>
2016-12-31 15:13:44 +0700

    DropzoneMvc Initial

---
+++ GdNet.Integrations.DropzoneMvc/Controllers/DropzoneAttachmentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+using GdNet.Integrations.DropzoneMvc.Models;
+
+namespace GdNet.Integrations.DropzoneMvc.Controllers
+{
+    public class DropzoneAttachmentController : Controller
+    {
+        public ActionResult GetComponent(Guid? referenceId)
+        {
+            var model = new DropzoneComponentViewModel()
+            {
+                ElementId = "filesZone",
+                TargetAction = Url.Action("Handle")
+            };
+
+            if (referenceId.HasValue && referenceId.Value != Guid.Empty)
+            {
+                model.ComponentId = referenceId.Value;
+            }
+
+            return PartialView("_DropzoneComponent", model);
+        }
+
+        [HttpPost]
+        public ActionResult Handle()
+        {
+            var attachmentInfos = UploadFiles();
+            return Json(attachmentInfos, JsonRequestBehavior.AllowGet);
+        }
+
+        private IEnumerable<string> UploadFiles()
+        {
+            var rootFolder = Server.MapPath(ConfigurationManager.AppSettings["TempFilesRoot"]);
+            var temporaryFolder = Request.Headers["X-ComponentId"];
+            return new DropzoneUploader(Request).UploadRequestFiles(rootFolder, temporaryFolder);
+        }
+    }
+}
---
+++ GdNet.Integrations.DropzoneMvc/Models/DropzoneComponentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GdNet.Integrations.DropzoneMvc.Models
+{
+    public class DropzoneComponentViewModel
+    {
+        public DropzoneComponentViewModel()
+        {
+            ComponentId = Guid.NewGuid();
+        }
+
+        public Guid ComponentId { get; set; }
+
+        public string ElementId { get; set; }
+
+        public string TargetAction { get; set; }
+    }
+}
