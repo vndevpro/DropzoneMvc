bd8984ff67b3faf28db43802b5b7455187cd4b51
juanonsoftware <juanonsoftware@gmail.com>
2023-10-27 11:09:14 +0700

    Demo for NetFramework & NetCore

--- DropzoneV5WithAspNetCore/Pages/Index.cshtml.cs
+++ DropzoneV5WithAspNetCore/Pages/Index.cshtml.cs
@@ -1,25 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DropzoneWithAspNetCore.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IHostEnvironment _environment;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IHostEnvironment environment)
         {
             _logger = logger;
+            _environment = environment;
         }
 
         public void OnGet()
         {
-
-        }
-
-        public IActionResult OnPost()
-        {
-            return new JsonResult(new { });
         }
     }
 }
